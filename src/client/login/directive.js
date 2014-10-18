5179efd3b2e1b5b40d334dbdba299af8ea2fa617
kpytang <kpytang@gmail.com>
2014-10-18 11:26:44 -0400

    update login page add partial views & routing

--- src/client/login/directive.js
+++ src/client/login/directive.js
@@ -1,14 +1,50 @@
 angular.module('globallinks.login.directive', [
 	'ui.router',
-	'login.template'
+	'login.template',
+	'login.base.template',
+	'login.resetpassword.template',
+	'login.signup.template'
 ]).config(function($stateProvider) {
-	$stateProvider.state({
-		name: 'login',
-		url: '/login',
+	var auth = {
+		abstract: true,
+		name: 'auth',
+		url: '/auth',
 		views: {
  			'main': {
 				templateUrl: 'login'
  			}
 		}
-	});
+	},
+	login = {
+		name: 'auth.login',
+		url: '/login',
+		views: {
+			'login': {
+				templateUrl: 'login/base'
+			}
+		}
+	},
+	resetPassword = {
+		name: 'auth.resetpassword',
+		url: '/resetpassword',
+		views: {
+ 			'login': {
+				templateUrl: 'login/resetpassword'
+ 			}
+		}
+	},
+	registerNewUser = {
+		name: 'auth.signup',
+		url: '/signup',
+		views: {
+ 			'login': {
+				templateUrl: 'login/signup'
+ 			}
+		}
+	}
+
+	$stateProvider.state(auth);
+	$stateProvider.state(login);
+	$stateProvider.state(resetPassword);
+	$stateProvider.state(registerNewUser);
 });
