6f5bf9fde8677bd23d2ab74e68a39e219a83eca7
kpytang <kpytang@gmail.com>
2014-10-18 17:03:17 -0400

    hook in logout

--- src/client/main.js
+++ src/client/main.js
@@ -2,4 +2,7 @@ angular.module('globallinks', [
 	'globallinks.login',
 	'globallinks.contact',
 	'globallinks.checkin'
-]);
+])
+.controller('mainCtrl', function($scope, LoginSvc){
+	$scope.auth = LoginSvc;
+});
