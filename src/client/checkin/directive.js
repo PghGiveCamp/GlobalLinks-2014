f6e26b1fb0f2698c5d02b26c66336d861444d3ec
kpytang <kpytang@gmail.com>
2014-10-18 15:35:42 -0400

    change checkin/checkout states

--- src/client/checkin/directive.js
+++ src/client/checkin/directive.js
@@ -7,10 +7,24 @@ angular.module('globallinks.checkin.directive', [
 		url: '/checkin',
 		views: {
  			'main': {
-				templateUrl: 'checkin'
+				templateUrl: 'checkin',
+				controller: 'CheckinCtrl'
  			}
 		}
 	}
 
 	$stateProvider.state(checkin);
+})
+.controller('CheckinCtrl', function($scope, ContactSvc){
+  $scope.volunteer = ContactSvc.currentUser;
+  $scope.checkin = function() {
+  	ContactSvc.currentUser.$checkin().then(function(){
+  		$scope.volunteer.status = 'started';
+  	});
+  }
+  $scope.checkout = function() {
+  	ContactSvc.currentUser.$checkout().then(function(){
+  		$scope.volunteer.status = 'done';
+  	});
+  }
 });
