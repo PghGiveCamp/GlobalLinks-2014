708ca4164a99fea8d362a2e68eb3b580b1e55088
jszwedko <jesse.szwedko@gmail.com>
2014-10-18 14:23:20 -0400

    Set --owner in created database during setup

--- lib/tasks/setup.rb
+++ lib/tasks/setup.rb
@@ -1,16 +1,23 @@
 require 'uri'
 
+def dburl
+  unless ENV.key?('DATABASE_URL')
+    $stderr.puts "\n---> ERROR: no DATABASE_URL set. <---"
+    $stderr.puts "---> ERROR: Check the README.md <---\n\n"
+    fail
+  end
+
+  @dburl ||= URI(ENV['DATABASE_URL'])
+end
+
 def dbname
   @dbname ||= begin
-    unless ENV.key?('DATABASE_URL')
-      $stderr.puts "\n---> ERROR: no DATABASE_URL set. <---"
-      $stderr.puts "---> ERROR: Check the README.md <---\n\n"
-      fail
-    end
-
-    dburl = URI(ENV['DATABASE_URL'])
     dburl.path.sub(/^\//, '')
   end
+end
+
+def dbuser
+  @dbuser ||= dburl.user || `whoami`
 end
 
 task :npm_install do
@@ -19,7 +26,7 @@ end
 
 task :psql_createdb do
   if `psql -l -t -A`.split("\n").grep(/^#{dbname}\|/).empty?
-    sh "createdb #{dbname}"
+    sh "createdb #{dbname} --owner #{dbuser}"
   end
 end
 
