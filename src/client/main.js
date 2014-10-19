175e806f0cfb08fa2fd5abdf5c02c3943ec6a32a
kpytang <kpytang@gmail.com>
2014-10-18 20:18:32 -0400

    styling site to match brand

--- src/client/main.js
+++ src/client/main.js
@@ -3,6 +3,65 @@ angular.module('globallinks', [
 	'globallinks.contact',
 	'globallinks.checkin'
 ])
-.controller('mainCtrl', function($scope, LoginSvc){
+.controller('mainCtrl', function($scope, LoginSvc, VolunteerQuotes, $location){
 	$scope.auth = LoginSvc;
-});
+	$scope.quotes = VolunteerQuotes;
+  $scope.isActive = function (viewLocation) { 
+      return viewLocation === $location.path();
+  };
+})
+.value('VolunteerQuotes', [
+  { 
+  	quote: "O​n behalf of GSK, we wish you great success. Know that we will continue our efforts to help support your wonderful organization.  Thanks to all of you for making us feel so welcome and sharing your mission.​", 
+  	name: "​Barb D."
+  },
+  { 
+  	quote: "It was truly a rewarding day for all of us.​", 
+  	name: "​Barb D."
+  },
+  { 
+  	quote: "O​n behalf of GSK, we wish you great success. Know that we will continue our efforts to help support your wonderful organization.  Thanks to all of you for making us feel so welcome and sharing your mission.​", 
+  	name: "​Barb D."
+  },
+  { 
+  	quote: "It was personally fulfilling to volunteer at Global Links seeing their work to help others do more, feel better, live longer.​", 
+  	name: "​Patti"
+  },
+  { 
+  	quote: "The experience was truly eye opening for all of us and we will definitely be looking to partner with you more in the future.  Your staff was friendly and very efficient at keeping us busy and leading our group.  We felt great on our way out the door, knowing that we had made a difference.​", 
+  	name: "Dara K."
+  },
+  { 
+  	quote: "I really enjoyed the time I spent there. I think what you do at Global Links is great and I really felt as though I made a difference when I volunteered there.​", 
+  	name: "​Jess B."
+  },
+  { 
+  	quote: "I really enjoy coming to Global Links. Such a wonderful and worthy cause.​", 
+  	name: "​Tammi V."
+  },
+  { 
+  	quote: "And thank YOU for always being so helpful and so much fun also! It really makes a difference for us volunteers and helps us to enjoy our time there.​", 
+  	name: "​Tammi V."
+  },
+  { 
+  	quote: "Global Links is so well organized that it makes the greatest possible use of volunteer time",
+  	name: "​Anonymous"
+  },
+  { 
+  	quote: "The best. I rarely do the others anymore because organization at the other place is not as nice as Global Links.  Everything is orderly and and straightforward so you feel like you have accomplished goals by the end of the volunteering...​", 
+  	name: "​​Anonymous"
+  },
+  { 
+  	quote: "Thanks for all you do to simultaneously reduce waste and save lives.  It's a beautiful thing.​", 
+  	name: "​​Anonymous"
+  },
+  { 
+  	quote: "I think you do an excellent job with organizing and keeping the volunteers buys and feeling useful.",
+  	name: "​​Anonymous"
+  },
+  {
+  	quote: "My volunteer experience has been exceptional.  Everything from the projects to the people has been great.",
+  	name: "​Anonymous"
+  }
+])
+;
